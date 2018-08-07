f1daccf3730ec5e1152968bedf125695a970c411
zfletch <zfletch2@gmail.com>
2018-08-07 14:54:25 -0400

    update dependencies

--- src/Footer.js
+++ src/Footer.js
@@ -7,13 +7,13 @@ class Footer extends Component {
       <footer className="footer bg-light">
         <div className="container">
           <span className="text-muted">
-            <a href="" target="_blank">New Document</a>
+            <a href="" target="_blank" rel="noopener noreferrer">New Document</a>
             {" "}&bull;{" "}
-            <a href="https://github.com/perseids-project/greek-latin-demixer" target="_blank">GitHub</a>
+            <a href="https://github.com/perseids-project/greek-latin-demixer" target="_blank" rel="noopener noreferrer">GitHub</a>
             {" "}&bull;{" "}
-            <a href="https://github.com/perseids-project/greek-latin-demixer/issues" target="_blank">Issues</a>
+            <a href="https://github.com/perseids-project/greek-latin-demixer/issues" target="_blank" rel="noopener noreferrer">Issues</a>
             {" "}&bull;{" "}
-            <a href="http://www.perseids.org/" target="_blank">Perseids</a>
+            <a href="http://www.perseids.org/" target="_blank" rel="noopener noreferrer">Perseids</a>
           </span>
           <br />
         </div>
