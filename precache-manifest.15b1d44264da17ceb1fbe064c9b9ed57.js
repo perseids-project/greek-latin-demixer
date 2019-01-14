4800d8654366b79ee8c052db15e66adfcd1a4235
zfletch <zfletch2@gmail.com>
2019-01-14 09:19:51 -0500

    Updates

---
+++ precache-manifest.15b1d44264da17ceb1fbe064c9b9ed57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "814602123976b68d5e95",
+    "url": "/greek-latin-demixer/static/js/runtime~main.81460212.js"
+  },
+  {
+    "revision": "f5cff26ebd7cd405f427",
+    "url": "/greek-latin-demixer/static/js/main.f5cff26e.chunk.js"
+  },
+  {
+    "revision": "3812726bea5acbffaba7",
+    "url": "/greek-latin-demixer/static/js/1.3812726b.chunk.js"
+  },
+  {
+    "revision": "f5cff26ebd7cd405f427",
+    "url": "/greek-latin-demixer/static/css/main.570dfd11.chunk.css"
+  },
+  {
+    "revision": "3812726bea5acbffaba7",
+    "url": "/greek-latin-demixer/static/css/1.6f7ef0a8.chunk.css"
+  },
+  {
+    "revision": "e810f8ae693d35bd12ca567e38902d63",
+    "url": "/greek-latin-demixer/index.html"
+  }
+];
