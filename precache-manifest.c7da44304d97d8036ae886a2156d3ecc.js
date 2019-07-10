de3373c349ffe16ee76a7a16437cc007e1fa6adc
zfletch <zfletch2@gmail.com>
2019-07-10 16:29:10 -0400

    Updates

---
+++ precache-manifest.c7da44304d97d8036ae886a2156d3ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24136f1fd5457f8cda9c877f895ae854",
+    "url": "/greek-latin-demixer/index.html"
+  },
+  {
+    "revision": "449f5ffe1ac58972b0b1",
+    "url": "/greek-latin-demixer/static/css/2.61f585c8.chunk.css"
+  },
+  {
+    "revision": "371d04801e9c4f7e7c35",
+    "url": "/greek-latin-demixer/static/css/main.44ddaeab.chunk.css"
+  },
+  {
+    "revision": "449f5ffe1ac58972b0b1",
+    "url": "/greek-latin-demixer/static/js/2.c1c8e43c.chunk.js"
+  },
+  {
+    "revision": "371d04801e9c4f7e7c35",
+    "url": "/greek-latin-demixer/static/js/main.373f4eb5.chunk.js"
+  },
+  {
+    "revision": "f7fa7878db9dfebbaa2c",
+    "url": "/greek-latin-demixer/static/js/runtime~main.8791682d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/greek-latin-demixer/precache-manifest.57177d383737e5c7588c3d486f6f90cf.js"
+  "/greek-latin-demixer/precache-manifest.c7da44304d97d8036ae886a2156d3ecc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/greek-latin-demixer/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/greek-latin-demixer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
