fd4edd7994032430fb33a6b1e202bd3c45be992c
zfletch <zfletch2@gmail.com>
2019-11-19 11:00:04 -0500

    configure react router and i18n

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { PerseidsHeader, PerseidsFooter } from 'perseids-react-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import 'perseids-react-components/build/css/index.css';
 
 import EditXml from '../EditXml';
 import InputXml from '../InputXml';
+import I18n from '../I18n';
 
 class App extends Component {
   constructor(props) {
@@ -25,12 +27,14 @@ class App extends Component {
     const { text } = this.state;
 
     return (
-      <>
+      <BrowserRouter>
         <PerseidsHeader>
-          Greek Latin Demixer
+          <I18n t="title" />
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="." target="_blank" rel="noopener noreferrer">New Document</a>
+              <a className="nav-link" href="." target="_blank" rel="noopener noreferrer">
+                <I18n t="newDoc" />
+              </a>
             </li>
           </ul>
         </PerseidsHeader>
@@ -47,7 +51,7 @@ class App extends Component {
           github="https://github.com/perseids-project/greek-latin-demixer"
           report="https://github.com/perseids-project/greek-latin-demixer/issues"
         />
-      </>
+      </BrowserRouter>
     );
   }
 }
